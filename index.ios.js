a68f402da0888f6b6104686b222c61226cfa455e
Jeremy Barbe <littlejssn@gmail.com>
2017-03-03 15:44:58 -0700

    capture image and pass to next route

--- index.ios.js
+++ index.ios.js
@@ -20,7 +20,7 @@ class Navigation extends Component {
        return <CameraView navigator={navigator} />
      }
      if(route.name == 'ViewCapture') {
-       return <ViewCapture navigator={navigator} />
+       return <ViewCapture navigator={navigator} image={route.imageData}/>
      }
   }
 
@@ -29,7 +29,9 @@ class Navigation extends Component {
     return (
       <Navigator
       initialRoute={{ name: 'CameraView' }}
-      renderScene={this.renderScene} />
+      renderScene={this.renderScene}
+      configureScene={(route, routeStack) =>
+      Navigator.SceneConfigs.FloatFromBottom} />
     );
   }
 }
@@ -40,8 +42,7 @@ class ViewCapture extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>YEY</Text>
-        <Image style={{width:'100%',height:'100%'}} source={{uri: '/Users/jeremybarbe/Library/Developer/CoreSimulator/Devices/B7296C79-92B3-43E5-8853-59A2B610C4B4/data/Containers/Data/Application/A1D9D3EE-FC0B-48CC-9AD8-E89CBE5C2882/Documents/8204BB2C-F313-4603-9719-87A1669AEBA4.jpg'}}/>
+        <Image style={{width:'100%',height:'100%'}} source={{uri: this.props.image}}/>
       </View>
     );
   }
@@ -49,6 +50,18 @@ class ViewCapture extends Component {
 
 
 class CameraView extends Component {
+
+  takePicture() {
+    this.camera.capture()
+      .then((data) => {
+        this.props.navigator.push({
+          name: 'ViewCapture', // Matches route.name
+          imageData: data.path
+        })
+      })
+      .catch(err => console.error(err));
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -66,16 +79,6 @@ class CameraView extends Component {
       </View>
     );
   }
-
-  takePicture() {
-    this.camera.capture()
-      .then((data) => console.log(data))
-      .catch(err => console.error(err));
-      this.props.navigator.push({
-        name: 'ViewCapture', // Matches route.name
-      })
-
-    }
 }
 
 const styles = StyleSheet.create({
